4546fe34d424940ff03f28e0b76824a1a9c6d16f
gojko <gojko@gojko.com>
2010-10-13 15:27:16 +0100

    added a section on comparison to other tools, website build tweaks, embed files works with macos or linux

--- resources/embedfiles.sh
+++ resources/embedfiles.sh
@@ -1,13 +1,24 @@
 cd $1
 
+#use gsed if available over sed, for Darwin to work
+SED=`which gsed`
+if [ $? -ne 0 ]; then 
+ SED=sed
+fi
+
+AWK=`which gawk`
+if [ $? -ne 0 ]; then 
+AWK=awk
+fi
+
 for c in *html; do
  echo $c
  mv $c $c.old
  #sed 's/<a[^>]*href="\([^\"]*\)"[^>]*><\/a>/\n\nAWKEMBED \1\n\n/g' $c.old  | 
- gsed 's/<body[^>]*>/\n\nAWKEMBED ..\/resources\/html_body_head.html\n\n/g' $c.old |  \
- gsed 's/<\/body[^>]*>/\n\nAWKEMBED ..\/resources\/html_body_foot.html\n\n/g'| \
- gsed 's/<\/head[^>]*>/\n\nAWKEMBED ..\/resources\/html_head.html\n\n/g' | \
- gawk '/AWKEMBED/{ while ((getline line < $2 ) > 0) print line; close ($2); next;} //{print $0}'  > $c 
- gsed -i 's/..\/images/images/g' $c
+ $SED 's/<body[^>]*>/\n\nAWKEMBED ..\/resources\/html_body_head.html\n\n/g' $c.old |  \
+ $SED 's/<\/body[^>]*>/\n\nAWKEMBED ..\/resources\/html_body_foot.html\n\n/g'| \
+ $SED 's/<\/head[^>]*>/\n\nAWKEMBED ..\/resources\/html_head.html\n\n/g' | \
+ $AWK '/AWKEMBED/{ while ((getline line < $2 ) > 0) print line; close ($2); next;} //{print $0}'  > $c 
+ $SED -i 's/..\/images/images/g' $c
  rm $c.old
 done    
