e9f94c93bc6d6b07f8cdc9c2a40a91bd8fa00a40
David de Florinier <dave@deflorinier.me.uk>
2010-10-20 08:23:33 +0100

    Create Ninjas4Hire project

---
+++ src/ruby/Ninjas4Hire/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '0c176ffe20192283387e0a9d9fb5893e1c403eeb6437c92dc204e1e43a94e87a228e28e39fe4cf7c0a8658ecfd4f10962c70d7c12e75d6f26ac48bb4378c8caf';
