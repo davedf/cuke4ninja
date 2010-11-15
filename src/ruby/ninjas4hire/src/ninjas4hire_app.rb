ebb266772a3e6339212cfb1327beeb14eb3d78ad
David de Florinier <dave@deflorinier.me.uk>
2010-11-15 22:07:52 +0000

    setup sinatra project ninjas4hire

---
+++ src/ruby/ninjas4hire/src/ninjas4hire_app.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'sinatra/base'
+#require (File.dirname(__FILE__)+ "/regexRouteFilter")
+
+class Ninjas4HireApp < Sinatra::Base
+#  register Sinatra::RegexpRouteFilter
+#	before_with_regexp(/^((?!\/config).)*$/) do
+#		if (@configuration.nil?)
+#		  redirect '/config', 301
+#		end
+#	end
+	get '/helloworld' do
+		"Hello world!"
+	end
+end
