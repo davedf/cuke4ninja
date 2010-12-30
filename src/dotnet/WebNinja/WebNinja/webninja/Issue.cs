af75e58f780ee56304c256d1424f3e08af6361e8
David de Florinier <dave@deflorinier.me.uk>
2010-12-30 22:33:49 +0000

    Added new dotnet project for Web Ninja

---
+++ src/dotnet/WebNinja/WebNinja/webninja/Issue.cs
@@ -0,0 +1,14 @@
+﻿namespace WebNinja.webninja
+{
+    public class Issue
+    {
+        public Issue(string title, string severity)
+        {
+            Title = title;
+            Severity = severity;
+        }
+        public string Title { get; private set; }
+        public string Severity { get; private set; }
+
+    }
+}
---
+++ src/dotnet/WebNinja/WebNinja/webninja/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebNinja.webninja
+{
+    public class User
+    {
+        public User(string name, string userId)
+        {
+            Name = name;
+            UserId = userId;
+        }
+        public string Name { get; private set; }
+
+        public string UserId { get; private set; }
+    }
+}
