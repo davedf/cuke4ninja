355eccdc0a87c38a4bece5ad97b1e13970711ace
David de Florinier <dave@deflorinier.me.uk>
2011-01-03 16:04:01 +0000

    web ninja skeleton classes

---
+++ src/dotnet/WebNinja/WebNinja/webninja/AssignedIssuesReport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebNinja.webninja
+{
+    public class AssignedIssuesReport
+    {
+        public AssignedIssuesReport(IList<Issue> issues)
+        {
+            Issues = issues;
+        }
+
+        public IList<Issue> Issues { get; private set; }
+
+        public int NumberOfIssues
+        {
+            get { return Issues == null ? 0 : Issues.Count; }
+        }
+    }
+}
---
+++ src/dotnet/WebNinja/WebNinja/webninja/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace WebNinja.webninja
+{    
+    public class UserRepository
+    {
+        private Dictionary<string, User> users;
+
+        public UserRepository ()
+        {
+            users = new Dictionary<string, User>();
+            Add(new User("Ninja1","n1"));
+            Add(new User("Ninja2", "n2"));
+        }
+
+        public void Add(User user)
+        {
+            users.Add(user.UserId, user);
+        }
+
+        public User FindByUserId(string userId)
+        {
+            return users[userId];
+        }
+    }
+}
