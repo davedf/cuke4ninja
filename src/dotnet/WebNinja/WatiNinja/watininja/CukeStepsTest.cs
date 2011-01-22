64485cddafd6bd6e15bffb2aa05aaa8b535e3e8b
David de Florinier <dave@deflorinier.me.uk>
2011-01-22 12:24:36 +0000

    Watininja

---
+++ src/dotnet/WebNinja/WatiNinja/watininja/CukeStepsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace WatiNinja.watininja
+{
+    [TestFixture]
+    public class CukeStepsTest
+    {
+        [Test]
+        public void SmokeTest()
+        {
+            new SmokeTestSteps().HelloWatin("WatiN"); 
+        } 
+    }
+}
---
+++ src/dotnet/WebNinja/WatiNinja/watininja/SmokeTestSteps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cuke4Nuke.Framework;
+using NUnit.Framework;
+using WatiN.Core;
+
+namespace WatiNinja.watininja
+{
+    public class SmokeTestSteps
+    {
+        [When("^I search for \"(.*)\" on google$")]
+        public void HelloWatin(string search)
+        {
+
+            using (var browser = new IE("http://www.google.com"))
+            {
+                browser.TextField(Find.ByName("q")).TypeText(search);
+                browser.Button(Find.ByName("btnG")).Click();
+
+                Assert.IsTrue(browser.ContainsText(search));
+            }
+        }
+    }
+}
